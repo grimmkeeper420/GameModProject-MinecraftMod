9115f50150a012945052bbe07276b358a60e34b7
four-b <grimmkeeper420@gmail.com>
2025-09-30 12:43:40 -0500

    trying to make a different structure that loads properly

---
+++ src/main/resources/structure/worldgen/structure/ruins_trial.java
@@ -0,0 +1,28 @@
+package structure.worldgen.structure;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ruins_trial {
+  "type": "minecraft:jigsaw",
+  "biomes": [
+    "minecraft:desert",
+    "minecraft:savanna"
+  ],
+  "step": "surface_structures",
+  "spawn_overrides": {},
+  "start_pool": "minecraft:trail_ruins/tower",
+  "size": 1,
+  "start_height": {
+    "above_bottom": 0
+  },
+  "start_jigsaw_name": "ruins_tower_trial",
+  "project_start_to_heightmap": "WORLD_SURFACE_WG",
+  "max_distance_from_center": 1,
+  "use_expansion_hack": true,
+  "liquid_settings": "ignore_waterlogging"
+}
